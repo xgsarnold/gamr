fffedba13da8e3c72b891b15241aa107ce7938d5
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-05-07 11:33:14 -0500

    Write tests for twitch game name and streams.

--- test/models/twitch_test.rb
+++ test/models/twitch_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class Twitch < ActiveRecord::Base
+  attr_reader :response
   def initialize()
     @results = JSON.parse(File.read("./mock_twitch.json"))
   end
@@ -15,10 +16,12 @@ class TwitchTest < ActiveSupport::TestCase
   end
 
   test "Twitch games have info" do
-
+    destiny = Twitch.new
+    assert_equal "Destiny", destiny.response["game"]["name"]
   end
 
   test "Twitch games have streams" do
-
+    destiny = Twitch.new
+    assert_equl "https://www.twitch.tv/planet_destiny", destiny.response["game"]["streams"][338]
   end
 end

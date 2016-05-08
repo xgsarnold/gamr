4cecfb15990177574b20d6c2b1ad7975ea48358d
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-05-07 20:44:46 -0500

    Activate bcript gem and write token generation method in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :session_tokens
+
+  def generate_token
+      SessionToken.create(user_id: id, token: SecureRandom.urlsafe_base64.to_s)
+  end
 end
